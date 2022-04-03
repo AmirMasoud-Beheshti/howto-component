285df5f6ad7653d15a95ac99d0595a5d5e230c1a
AmirMasoud Beheshti <am.beheshti@deltaban.local>
2022-04-03 22:32:06 +0430

    complete accordion component

--- components/Accordions/Accordion.tsx
+++ components/Accordions/Accordion.tsx
@@ -1,23 +1,33 @@
-import React,{ useState } from "react";
+import clsx from "clsx";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
-export interface AccordionProps{
-  title:string
-  content:string
+import styles from "./Accordion.module.scss";
+export interface AccordionProps {
+  title: string;
+  content: string;
+  onToggle: () => void
+  active:boolean
 }
-const Accordion = (props:AccordionProps) => {
-  const {title, content} = props
-  const [isOpen, setIsOpen] = useState(false)
+const Accordion = (props: AccordionProps) => {
+  const { title, content, onToggle,active } = props;
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    onToggle();
   };
   return (
-    <div key={title}>
-      <div>
-        <button onClick={handleClick}>{title}</button>
-        {isOpen ? <FaMinus /> : <FaPlus />}
+    <div key={title} className={styles["accordion"]}>
+      <div className={styles["accordion__action"]} onClick={handleClick}>
+        {title}
+        {active ? <FaMinus /> : <FaPlus />}
       </div>
-      <div>{content}</div>
+      <div
+        className={clsx(
+          styles["accordion__content"],
+          active && styles["accordion__content--active"]
+        )}
+      >
+        {content}
+      </div>
     </div>
   );
 };
--- components/Accordions/Accordions.tsx
+++ components/Accordions/Accordions.tsx
@@ -7,20 +7,24 @@ export interface AccordionsProps {
 }
 const Accordions = (props: AccordionsProps) => {
   const { items } = props;
-  const [isOpen, setIsOpen] = useState(false);
- 
+  const [clicked, setClicked] = useState<number | null>(null);
+  const handleToggle = (index) => {
+    if (clicked === index) return setClicked(null);
+    setClicked(index);
+  };
 
-  const renderdAccordions = items.map((item) => {
+  const renderdAccordions = items.map((item, index) => {
     return (
       <Accordion
         key={item.title}
         title={item.title}
         content={item.content}
-
+        onToggle={() => handleToggle(index)}
+        active={clicked === index}
       />
     );
   });
-  return <div>{renderdAccordions}</div>;
+  return <>{renderdAccordions}</>;
 };
 
 export default Accordions;
